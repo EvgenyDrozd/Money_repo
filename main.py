af9d4e022bc6b6f964563f700169b0481ac62f27
Evgeni <cryptodrozd1982@gmail.com>
2025-10-17 09:09:47 +0000

    Add magics methods for +

--- main.py
+++ main.py
@@ -4,7 +4,23 @@ class Money:
         self.kop = kop % 100
 
 
+    def get_rub(self):
+        return self.rub
+
+    def get_kop(self):
+        return self.kop
+
+    def __add__(self, other):
+        return Money(self.rub + other.rub, self.kop + other.kop)
+
+    def __str__(self):
+        return f'Monney({self.rub}, {self.kop:02d})'
+
 if __name__ == "__main__":
     money1 = Money(20, 120)
     money2 = Money(30,30)
     print(f'{money1.rub = }, {money1.kop = }')
+    print(money1)
+    print(money2)
+    money3 = money1 + money2
+    print(money3)
